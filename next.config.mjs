58eca2ac1979d5006a6b1b5a5de65390f5d837d4
Chioma <anigboguchioma31@gmail.com>
2024-07-31 22:47:07 +0100

    live version 2.000

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-    output: 'export',
+const nextConfig = {};
 
-    
-}
-   
-  export default nextConfig
+export default nextConfig; 
