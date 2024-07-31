873145854bbb198c7ed274fce4e3e110be58dc33
Chioma <anigboguchioma31@gmail.com>
2024-07-31 22:21:19 +0100

    reverting back

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,6 @@ export default function Home() {
     <>
       <div  className="">
         <Landing />
-        <div>404.html,1722440273447,b7bab6b83fa074653ff28c8d2a64135d3434575f70a12ab3d3ba8080461b9537 index.html,1722440276767,6e3deafe735a14203714c500cb01ddb923c97f401e65d10769653c013eb3cd89
-        </div>
       </div>
     </>
   )
