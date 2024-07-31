4353a98c1d179ce463f2acd4809019bb7d9eb6d0
Chioma <anigboguchioma31@gmail.com>
2024-07-31 20:55:02 +0100

    deploying

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,12 @@ import Landing from "@/screens/landing/landing";
 export default function Home() {
 
 
-  const productLaunched = process.env.NEXT_PUBLIC_PRODUCT_LAUNCHED === 'true';
-
-
-
   return (
     <>
       <div  className="">
         <Landing />
+        <div>404.html,1722440273447,b7bab6b83fa074653ff28c8d2a64135d3434575f70a12ab3d3ba8080461b9537 index.html,1722440276767,6e3deafe735a14203714c500cb01ddb923c97f401e65d10769653c013eb3cd89
+        </div>
       </div>
     </>
   )
